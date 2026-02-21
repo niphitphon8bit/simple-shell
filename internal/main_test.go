1ea7a93b1ead767c6514574b90588489945ce51b
niphitphon8bit <niphitphon.nt@gmail.com>
2026-02-21 22:09:28 +0700

    update unit test to support cd command with many arguments

--- internal/main_test.go
+++ internal/main_test.go
@@ -2,21 +2,43 @@ package internal
 
 import (
 	"errors"
+	"os"
 	"os/exec"
 	"testing"
 )
 
-func TestExistCommand(t *testing.T) {
+func TestExecInput(t *testing.T) {
 	tests := []struct {
-		name           string
-		givenInput     string
-		expectedOutput string
-		expectedErr    error
+		name        string
+		givenInput  string
+		expectedDir string
+		expectedErr error
 	}{
+		{
+			name:        "happy: ls",
+			givenInput:  "ls",
+			expectedErr: nil,
+		},
+		{
+			name:        "happy: ls -l -a",
+			givenInput:  "ls -l -a",
+			expectedErr: nil,
+		},
 		{
 			name:        "happy: exit",
 			givenInput:  "exit",
 			expectedErr: ErrExit,
+		},
+		{
+			name:        "happy: cd",
+			givenInput:  "cd",
+			expectedErr: ErrNoPath,
+		},
+		{
+			name:        "happy: cd /",
+			givenInput:  "cd /",
+			expectedErr: nil,
+			expectedDir: "/",
 		},
 		{
 			name:        "bad: wrong spell command",
@@ -28,8 +50,20 @@ func TestExistCommand(t *testing.T) {
 	for _, eachTest := range tests {
 		t.Run(eachTest.name, func(t *testing.T) {
 			// run each test here
+
 			if err := ExecInput(eachTest.givenInput); !errors.Is(err, eachTest.expectedErr) {
 				t.Errorf("execInput() error = %v, want Err %v", err, eachTest.expectedErr)
+			}
+
+			// test for change directory only
+			if eachTest.expectedDir != "" {
+				curDir, err := os.Getwd()
+				if err != nil {
+					t.Errorf("Failed to get new directory: %v", err)
+				}
+				if eachTest.expectedDir != curDir {
+					t.Errorf("Failed to change to desired directory.")
+				}
 			}
 		})
 	}

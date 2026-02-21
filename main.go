e6d01e8cea589568fb5cc95c46665e367c846eb4
niphitphon8bit <niphitphon.nt@gmail.com>
2026-02-21 19:28:49 +0700

    add switch case to support exit and cd with argument

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"os"
 	"os/exec"
@@ -35,8 +36,19 @@ func execInput(input string) error {
 	// Split the input to separate the command and the arguments.
 	args := strings.Split(input, " ")
 
+	switch args[0] {
+	case "cd":
+		// 'cd' to home dir with empty path not yet supported.
+		if len(args) < 2 {
+			return errors.New("path required.")
+		}
+		return os.Chdir(args[1])
+	case "exit":
+		os.Exit(0)
+	}
+
 	// Prepare the command to execute.
-	cmd := exec.Command(args[0], args[1])
+	cmd := exec.Command(args[0], args[1:]...)
 
 	// Set the correct output device.
 	cmd.Stderr = os.Stderr

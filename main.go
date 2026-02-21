bbd775dafa081578cbddcc903f21be92b80d8dde
niphitphon8bit <niphitphon.nt@gmail.com>
2026-02-21 19:13:19 +0700

    added simple execute function

--- main.go
+++ main.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Print("> ")
+
+		// Read the keyword input.
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+
+		// Handle the execution of the input.
+		if err = execInput(input); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+	}
+}
+
+// Execute command function
+func execInput(input string) error {
+	// Remove th newline character.
+	input = strings.TrimSuffix(input, "\n")
+
+	// Prepare the command to execute.
+	cmd := exec.Command(input)
+
+	// Set the correct output device.
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+
+	// Execute the command and return the error.
+	return cmd.Run()
 }
